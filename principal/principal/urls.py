4056a198e4f425802e5d68a472b60fd2504f2dca
Tony-karhub-notebook <tony4etlvp@gmail.com>
2024-10-24 01:19:40 -0300

    Primeiro commit codigo django base

---
+++ principal/principal/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+from estoque import views  # Importando a view diretamente do app estoque
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('estoque/', include('estoque.urls')),
+    path('', views.index),  # Define uma rota para a URL raiz
+]
